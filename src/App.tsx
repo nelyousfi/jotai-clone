c142f7a73c23aac1f14b4d88fa7cbe94b79bdc65
Naoufal Elyoussoufi <n.elyousfi@gmail.com>
2022-05-02 21:13:29 +0200

    Play with the Jotai readme example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { atom, useAtom } from 'jotai'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const countAtom = atom(0)
+const doubleCountAtom = atom((get) => get(countAtom) * 2)
+const urlAtom = atom('https://jsonplaceholder.typicode.com/todos/1')
+const fetchUrlAtom = atom(async (get) => {
+    const response = await fetch(get(urlAtom))
+    return await response.json()
+})
+const decrementCountAtom = atom(
+    (get) => get(countAtom),
+    (get, set) => set(countAtom, get(countAtom) - 1)
+)
+const multiplyCountAtom = atom(null, (get, set, by: number) =>
+    set(countAtom, get(countAtom) * by)
+)
+const todoAtom = atom(undefined)
+const fetchTodoAtom = atom(
+    (get) => get(todoAtom),
+    async (get, set) => {
+        const response = await fetch(
+            'https://jsonplaceholder.typicode.com/todos/1'
+        )
+        set(todoAtom, await response.json())
+    }
+)
+
+function Counter() {
+    const [count, setCount] = useAtom(countAtom)
+    const [, decrementCount] = useAtom(decrementCountAtom)
+    const [doubleCount] = useAtom(doubleCountAtom)
+    const [, multiplyCount] = useAtom(multiplyCountAtom)
+
+    return (
+        <div>
+            <p>{count}</p>
+            <p>{doubleCount}</p>
+            <button onClick={() => setCount((c) => c + 1)}>increment</button>
+            <button onClick={() => decrementCount()}>decrement</button>
+            <button onClick={() => multiplyCount(10)}>multiply</button>
+        </div>
+    )
 }
 
-export default App;
+function Status() {
+    const [json] = useAtom(fetchUrlAtom)
+
+    return <div>{JSON.stringify(json, undefined, 2)}</div>
+}
+
+function App() {
+    const [todo, fetchTodo] = useAtom(fetchTodoAtom)
+
+    return (
+        <div className="App">
+            <Counter />
+            <p style={{ color: 'red' }}>{JSON.stringify(todo, null, 2)}</p>
+            <button onClick={fetchTodo}>Fetch Todo</button>
+            <Suspense fallback={<p>Loading ...</p>}>
+                <Status />
+            </Suspense>
+        </div>
+    )
+}
+
+export default App
