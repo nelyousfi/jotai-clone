aa84fafa9aa8b0afb2fafd08da89dd6e686b2c9c
Naoufal Elyoussoufi <n.elyousfi@gmail.com>
2022-05-08 18:02:11 +0200

    Cleanup the code

--- src/AppTwo.tsx
+++ src/AppTwo.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { atom, useAtom } from './useAtom'
+import { useAtom } from 'jotai'
+
+type Getter<Value> = (atom: Atom<Value>) => Value
+
+type Setter<Value> = (atom: Atom<Value>, newValue: Value) => void
+
+type Atom<Value> = {
+    toString: () => string
+    init: Value
+    read: (get: Getter<Value>) => Value
+    write: (
+        get: Getter<Value>,
+        set: Setter<Value>,
+        update: (oldValue: Value) => Value
+    ) => void
+}
+
+let keyCount = 0
+
+function atom<Value>(initialValue: Value): Atom<Value> {
+    const config = {
+        toString: () => `atom${++keyCount}`,
+        init: initialValue,
+        read: (get) => get(config),
+        write: (get, set, update) => set(config, update(get(config))),
+    } as Atom<Value>
+    return config
+}
+
+function useAtomValue<Value>(atom: Atom<Value>): Value {
+    return atom.init
+}
 
 const countAtom = atom(0)
+const ageAtom = atom(30)
 
 function App() {
     const [count, setCount] = useAtom(countAtom)
+    const age = useAtomValue(ageAtom)
 
     return (
         <div className="App">
             <p>Count: {count}</p>
             <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+            <p>age: {age}</p>
         </div>
     )
 }
