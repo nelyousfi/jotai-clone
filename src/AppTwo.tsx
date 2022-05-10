da7ee3548f163520f9a368f70a6bb42ec4d70fd7
Naoufal Elyoussoufi <n.elyousfi@gmail.com>
2022-05-10 21:22:05 +0200

    WIP

--- src/AppTwo.tsx
+++ src/AppTwo.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import { useAtom } from 'jotai'
+import React, {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useReducer,
+} from 'react'
+import { useSetAtom } from 'jotai'
 
 type Getter<Value> = (atom: Atom<Value>) => Value
 
 type Setter<Value> = (atom: Atom<Value>, newValue: Value) => void
 
-type Atom<Value> = {
+type Atom<Value = unknown> = {
     toString: () => string
     init: Value
     read: (get: Getter<Value>) => Value
@@ -28,8 +34,130 @@ function atom<Value>(initialValue: Value): Atom<Value> {
     return config
 }
 
+type AtomState<Value = unknown> = {
+    v: Value
+}
+
+type Mounted = {
+    listeners: Set<() => void>
+}
+
+const createStore = () => {
+    const committedAtomStateMap = new WeakMap<Atom, AtomState>()
+    const mountedMap = new WeakMap<Atom, Mounted>()
+
+    function getAtomState<Value>(
+        atom: Atom<Value>
+    ): AtomState<Value> | undefined {
+        const atomState = committedAtomStateMap.get(atom as Atom) as
+            | AtomState<Value>
+            | undefined
+        if (atomState) {
+            return atomState
+        }
+        const initAtomState = {
+            v: atom.init,
+        }
+        committedAtomStateMap.set(atom as Atom, initAtomState)
+        return initAtomState
+    }
+
+    function readAtomState<Value>(atom: Atom<Value>): AtomState<Value> {
+        const atomState = getAtomState(atom)
+        if (atomState) {
+            return atomState
+        }
+        throw new Error('No atom found!')
+    }
+
+    function readAtom<Value>(atom: Atom<Value>): AtomState<Value> {
+        return readAtomState(atom)
+    }
+
+    function writeAtom() {}
+
+    function mountAtom<Value>(atom: Atom<Value>): Mounted {
+        const mounted = {
+            listeners: new Set<() => {}>(),
+        }
+        mountedMap.set(atom as Atom, mounted)
+        return mounted
+    }
+
+    function addAtom<Value>(atom: Atom<Value>) {
+        let mounted = mountedMap.get(atom as Atom)
+        if (!mounted) {
+            mounted = mountAtom(atom)
+        }
+        return mounted
+    }
+
+    function deleteAtom<Value>(atom: Atom<Value>) {
+        mountedMap.delete(atom as Atom)
+    }
+
+    function subscribeAtom<Value>(
+        atom: Atom<Value>,
+        callback: () => void
+    ): () => void {
+        const mounted = addAtom(atom)
+        if (!mounted) {
+            throw new Error('No atom found!')
+        }
+        mounted.listeners.add(callback)
+        return () => {
+            mounted.listeners.delete(callback)
+            deleteAtom(atom)
+        }
+    }
+
+    return {
+        r: readAtom,
+        w: writeAtom,
+        s: subscribeAtom,
+    }
+}
+
+const ScopeContext = createContext({
+    s: createStore(),
+})
+
 function useAtomValue<Value>(atom: Atom<Value>): Value {
-    return atom.init
+    const { s: store } = useContext(ScopeContext)
+
+    const getAtomValue = useCallback(() => {
+        const atomState = store['r'](atom)
+        if ('v' in atomState) {
+            return atomState.v
+        }
+        throw new Error('no atom value')
+    }, [atom, store])
+
+    const [value, renderIfChanged] = useReducer(
+        (prevValue) => {
+            const nextValue = getAtomValue()
+            if (Object.is(prevValue, nextValue)) {
+                return prevValue
+            }
+            return nextValue
+        },
+        undefined,
+        () => {
+            return undefined
+        }
+    )
+
+    useEffect(() => {
+        const unsubscribe = store['s'](atom, renderIfChanged)
+        renderIfChanged()
+        return unsubscribe
+    }, [atom, store])
+
+    return value
+}
+
+function useAtom<Value, Update, Result>(atom: Atom<Value>): [any, any] {
+    return [useAtomValue(atom), useSetAtom(atom)]
 }
 
 const countAtom = atom(0)
@@ -42,7 +170,9 @@ function App() {
     return (
         <div className="App">
             <p>Count: {count}</p>
-            <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+            <button onClick={() => setCount((c: any) => c + 1)}>
+                Increment
+            </button>
             <p>age: {age}</p>
         </div>
     )
